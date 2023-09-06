b0896c07135cc7eac7784ae3de444b05d4f36adc
Eray <77186648+Eraycak@users.noreply.github.com>
2023-09-06 12:48:58 +0300

    Icon and Logo

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -10,19 +10,26 @@ class StartScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 200, height: 200),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+            height: 200,
+            color: const Color.fromARGB(150, 255, 255,
+                255), //*alpha is changed to add some transparency
+          ),
           const SizedBox(height: 60),
           const Text(
             'Start Screen',
             style: TextStyle(fontSize: 30, color: Colors.white),
           ),
           const SizedBox(height: 20),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text('Start Quiz'),
+            icon: const Icon(Icons.play_arrow_sharp),
+            label: const Text('Start Quiz'),
           ),
         ],
       ),
